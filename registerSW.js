46563486fb3fb75983a3f7a12b6a78270c8e727c
Kasfex <kasfex@gmail.com>
2026-05-03 23:54:40 +0100

    Deploy GitHub Pages

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Simple-Personal-Workout-Tracker-PWA/sw.js', { scope: '/Simple-Personal-Workout-Tracker-PWA/' })})}
